25e9ed643ceea6583b2ab352d7e29a2d2d839b67
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2021-11-04 13:54:47 +0530

    dev

--- src/js/terminal/commands/about.js
+++ src/js/terminal/commands/about.js
@@ -3,18 +3,16 @@ let exports = {};
 exports.aboutCommand = () => {
   const age = new Date().getFullYear() - 2005;
   return (
-    "\nHey! My name is Shiv Patil, and I am a student programmer currently living in India. \
-I am currently " +
+    "\nHey! My name is Shiv Patil, and I am an Indian student programmer. \
+I'm roughly " +
     age.toString() +
-    " years old, and am programming for about " +
-    (age - 12).toString() +
-    " years now.\n\n\
-I have worked mostly with python, and am well-versed with the language.\n\
-Most of my projects were made using the Kivy GUI framework in python, \
-and I have also done various freelance projects using it.\n\n\
-I have also dabbled with some other languages such as C#, C++, JavaScript and have beginner-level experience with all of them.\n\n\
-Outside Computer Science, I love Physics and enjoy intellectual problems related to it.\n\
-I also love to hear about the latest technology news and its workings!\n"
+    " years old and have been programming for 4 years.\n\n\
+I've largely worked with Python and am quite acquainted with it.\n\
+The Kivy GUI framework in Python was used in the majority of my projects, \
+and I've also completed several freelancing tasks with it.\n\n\
+Aside from Python, I've experimented in other languages like C# and C++ and have some beginner-level knowledge with them.\n\
+I am also well-versed in JavaScript.\n\n\
+Discord ID: KrYmZiN#0311\n"
   );
 };
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,30 @@
 <template>
-  <div class="home">
-    <div class="parallax-bg"> </div>
+  <div id="snackbar" class="snackbar">Discord tag copied</div>
+  <div class="home" ref="home">
+    <img src="@/assets/background__inner.jpg" class="bg__inner" alt="website background"/>
     <div class="nav">
-      <div class="nav__item box">About</div>
-      <div class="nav__item box">Projects</div>
-      <div class="nav__item box">Contact</div>
+      <router-link class="nav__item box" to="/">Terminal</router-link>
+      <div class="nav__item box" @click="aboutButton">About</div>
+      <div class="nav__item box" @click="projectsButton">Projects</div>
+      <div class="nav__item box" @click="discordSnack">Contact</div>
     </div>
     <div class="hero__wrapper">
       <div class="hero box">
         <div class="hero__title">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam molestie
-          est eu purus consequat, non sodales ante iaculis.
+          Website work in progress :)
         </div>
-        <img src="@/assets/hero__image.png" class="hero__image">
+        <div class="hero__image__wrapper box">
+          <img
+            src="@/assets/hero__image.png"
+            @load="bgLoaded"
+            class="hero__image"
+            alt="picture"
+          />
+        </div>
       </div>
     </div>
   </div>
+  <div id="loader__wrapper"><div class="loader"></div></div>
 </template>
 
 <script>
@@ -24,7 +33,23 @@
 export default {
   name: "Home",
   components: {},
-  methods: {},
+  methods: {
+    bgLoaded() {
+      document.getElementById("loader__wrapper").style.display = "none";
+    },
+    discordSnack() {
+      navigator.clipboard.writeText("KrYmZiN#0311");
+      let sn = document.getElementById("snackbar");
+      sn.className = "snackbar show";
+      setTimeout(() => {sn.className = "snackbar"}, 2000);
+    },
+    projectsButton() {
+      window.location = "https://www.github.com/Shiv-Patil";
+    },
+    aboutButton() {
+      window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    }
+  },
   mounted() {},
 };
 </script>
@@ -32,18 +57,18 @@ export default {
 <style scoped>
 .box {
   border-radius: 10px;
-  background-color: rgba(1, 1, 1, .2);
-  backdrop-filter: blur(5px) contrast(130%);
-  box-shadow: 12px 12px 16px 0 rgba(0, 0, 0, 0.25),
-  -7px -7px 12px 0 rgba(255, 255, 255, 0.4);
+  background-color: rgba(94, 104, 121, 0.05);
+  box-shadow: 12px 12px 20px rgba(94, 104, 121, 0.4),
+    -4px -4px 14px rgba(252, 252, 252, 0.8);
 }
 
 .home {
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 1500px;
   overflow-x: hidden;
   overflow-y: auto;
-  perspective: 24px;
+  /*perspective: 24px;*/
+  min-width: 372px;
 }
 
 .nav {
@@ -58,6 +83,28 @@ export default {
   padding: 8px;
   user-select: none;
   color: #212131;
+  text-decoration: none;
+}
+
+.snackbar {
+  display: none;
+  min-width: 250px;
+  margin-left: -125px;
+  background-color: #efefef;
+  color: #333;
+  text-align: center;
+  border-radius: 2px;
+  padding: 16px;
+  position: fixed;
+  z-index: 1;
+  left: 50%;
+  bottom: 30px;
+}
+
+.snackbar.show {
+  display: block;
+  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
+  animation: fadein 0.5s, fadeout 0.5s 2.5s;
 }
 
 .nav__item:hover {
@@ -65,33 +112,42 @@ export default {
 }
 
 .nav__item:active {
-  box-shadow: none;
+  box-shadow: inset 6px 6px 10px rgba(94, 104, 121, 0.4),
+    inset -3px -3px 7px rgba(255, 255, 255, 0.8);
 }
 
-.parallax-bg {
+.bg__inner {
   position: absolute;
   top: 0;
   right: 0;
   left: 0;
-  height: 3000px;
-  background-attachment: scroll;
-  background-image: url("../assets/hero__bg.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  transform: translateZ(-18px) scale(1.75);
+  bottom: 0;
+  width: 100%;
+  height: max(1500px, 100vh);
 }
+
+/*.parallax-bg {*/
+/*  position: absolute;*/
+/*  top: 0;*/
+/*  right: 0;*/
+/*  left: 0;*/
+/*  height: 2000px;*/
+/*  width: 100%;*/
+/*  transform: translateZ(-18px) scale(1.75);*/
+/*}*/
 
 .hero__wrapper {
   height: 580px;
   transform: translateZ(0);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .hero {
   position: absolute;
-  top: 5px;
-  left: 40px;
-  right: 40px;
-  bottom: 40px;
+  width: min(92%, 1200px);
+  height: 100%;
 }
 
 .hero__title {
@@ -100,19 +156,110 @@ export default {
   left: 65%;
   width: 50%;
   transform: translateX(-50%) translateY(-50%);
-  font-size: xx-large;
+  font-size: 30px;
   font-weight: bold;
   color: #212131;
 }
 
-.hero__image {
+.hero__image__wrapper {
   position: absolute;
-  height: 50%;
+  height: 290px;
+  width: 290px;
   top: 25%;
-  left: 15%;
+  left: 10%;
   border-radius: 50%;
-  background: inherit;
-  box-shadow: 10px 10px 14px 0 rgba(0, 0, 0, 0.17),
-  -5px -5px 8px 0 rgba(200, 200, 200, 0.17);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.hero__image {
+  width: 80%;
+}
+
+@media screen and (max-width: 1100px) {
+  .hero__title {
+    font-size: max(25px, min(6vw, 30px));
+    top: 75%;
+    left: 50%;
+    width: 80%;
+  }
+
+  .hero__image__wrapper {
+    height: 250px;
+    width: 250px;
+    top: 12%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+}
+
+#loader__wrapper {
+  background-color: #efefef;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+
+.loader,
+.loader:after {
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+}
+.loader {
+  font-size: 10px;
+  border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-left: 1.1em solid #111111;
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+@-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes fadein {
+  from {bottom: 0; opacity: 0;}
+  to {bottom: 30px; opacity: 1;}
+}
+
+@keyframes fadein {
+  from {bottom: 0; opacity: 0;}
+  to {bottom: 30px; opacity: 1;}
+}
+
+@-webkit-keyframes fadeout {
+  from {bottom: 30px; opacity: 1;}
+  to {bottom: 0; opacity: 0;}
+}
+
+@keyframes fadeout {
+  from {bottom: 30px; opacity: 1;}
+  to {bottom: 0; opacity: 0;}
 }
 </style>
