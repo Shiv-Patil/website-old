7a5ad2aa893a99329394c5047a5483b6bd5d5f8a
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2021-11-11 14:38:39 +0530

    landing animations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,61 @@
 <template class="home">
   <div id="snackbar" class="snackbar">Discord tag copied</div>
-    <img src="@/assets/background__inner.jpg" class="bg__inner" alt="website background"/>
-    <div class="nav">
-      <router-link class="nav__item box" to="/">Terminal</router-link>
-      <div class="nav__item box" @click="aboutButton">About</div>
-      <div class="nav__item box" @click="projectsButton">Projects</div>
-      <div class="nav__item box" @click="discordSnack">Contact</div>
+  <img
+    src="@/assets/background__inner.jpg"
+    class="bg__inner"
+    alt="website background"
+  />
+  <div class="nav">
+    <router-link class="nav__item neu" to="/" @click="nav_item_click(this)"
+    >Terminal
+      <div class="nav__item__indicator"></div
+      >
+    </router-link>
+    <div
+      class="nav__item neu"
+      @click="
+        aboutButton();
+        nav_item_click(this);
+      "
+    >
+      About
+      <div class="nav__item__indicator"></div>
     </div>
-    <div class="hero__wrapper">
-      <div class="hero box">
-        <div class="hero__title">
-          Website work in progress :)
-        </div>
-        <div class="hero__image__wrapper box">
-          <img
-            src="@/assets/hero__image.png"
-            @load="bgLoaded"
-            class="hero__image"
-            alt="picture"
-          />
-        </div>
+    <a
+      class="nav__item neu"
+      href="https://www.github.com/Shiv-Patil"
+      @click="nav_item_click(this)"
+    >
+      Projects
+      <div class="nav__item__indicator"></div>
+    </a>
+    <div
+      class="nav__item neu"
+      @click="
+        discordSnack();
+        nav_item_click(this);
+      "
+    >
+      Contact
+      <div class="nav__item__indicator"></div>
+    </div>
+  </div>
+  <div class="hero__wrapper">
+    <div class="hero neu">
+      <div class="hero__title">Website work in progress :)</div>
+      <div class="hero__image__wrapper neu">
+        <img
+          src="@/assets/hero__image.png"
+          @load="bgLoaded"
+          class="hero__image"
+          alt="picture"
+        />
       </div>
     </div>
-  <div id="loader__wrapper"><div class="loader"></div></div>
+  </div>
+  <div id="loader__wrapper">
+    <div class="loader"></div>
+  </div>
 </template>
 
 <script>
@@ -30,61 +63,142 @@
 
 export default {
   name: "Home",
+  data() {
+    return {
+      scrollEventTimeout: 400,
+      scrollEventLastCalled: Date.now()
+    };
+  },
   components: {},
   methods: {
     bgLoaded() {
       document.getElementById("loader__wrapper").style.display = "none";
+      this.pageScrollTimeout();
     },
     discordSnack() {
       navigator.clipboard.writeText("KrYmZiN#0311");
       let sn = document.getElementById("snackbar");
       sn.className = "snackbar show";
-      setTimeout(() => {sn.className = "snackbar"}, 2000);
-    },
-    projectsButton() {
-      window.location = "https://www.github.com/Shiv-Patil";
+      setTimeout(() => {
+        sn.className = "snackbar";
+      }, 2000);
     },
     aboutButton() {
-      window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    },
+    nav_item_click() {
+    },
+    pageScroll() {
+      if (Date.now() - this.scrollEventLastCalled > this.scrollEventTimeout) {
+        this.pageScrollTimeout();
+        this.scrollEventLastCalled = Date.now();
+      }
+    },
+    pageScrollTimeout() {
+      console.log("timeout scroll");
+      const vh = window.innerHeight;
+      document.querySelectorAll(".neu").forEach((ele) => {
+        if (!ele.classList.contains("scale_and_fade_in"))
+          if (ele.getBoundingClientRect().top < vh - 10)
+            ele.classList.add("scale_and_fade_in");
+      });
     }
   },
-  mounted() {},
+  mounted() {
+    window.addEventListener("scroll", this.pageScroll);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.pageScroll);
+  }
 };
 </script>
 
 <style scoped>
-.box {
-  border-radius: 10px;
-  background-color: rgba(94, 104, 121, 0.05);
-  box-shadow: 12px 12px 20px rgba(94, 104, 121, 0.4),
-    -4px -4px 14px rgba(252, 252, 252, 0.8);
-}
-
 .home {
-  /*perspective: 24px;*/
-  min-width: 300px;
+  min-width: 200px;
 }
 
 .nav {
   display: flex;
   justify-content: center;
   padding: 5px;
-  transform: translateZ(0);
+  margin: max(2px, min(0.5vw, 4px)) 0;
+  min-width: 180px;
+}
+
+.scale_and_fade_in {
 }
 
 .nav__item {
-  font-size: max(10px, min(4vw, 16px));
-  margin: max(4px, min(2vw, 8px));
+  position: relative;
+  border-radius: 6px;
+  transform: scale(0.97);
+  font-size: max(8px, min(4vw, 16px));
+  margin: 8px max(4px, min(2vw, 8px));
   padding: max(4px, min(2vw, 8px));
   user-select: none;
   color: #212131;
   text-decoration: none;
-  font-family: Poppins,Montserrat,serif;
+  font-family: Poppins, Montserrat, serif;
+}
+
+.nav__item::before {
+  content: "";
+  border-radius: 6px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  box-shadow: 4px 4px 8px rgba(94, 104, 121, 0.4),
+  -2px -2px 5px rgba(252, 252, 252, 0.8);
+  background-color: rgba(94, 104, 121, 0.05);
+  opacity: 0;
+  transform: scale(1.04166);
+}
+
+.nav__item.scale_and_fade_in {
+  animation: scalein 0.7s linear forwards;
+}
+
+.nav__item.scale_and_fade_in::before {
+  animation: fadein 1s ease-in forwards, scalein 1s linear forwards;
+}
+
+.nav__item:hover {
+  cursor: pointer;
+}
+
+.nav__item:active::before {
+  box-shadow: inset 2px 2px 4px rgba(94, 104, 121, 0.4),
+  inset -2px -2px 4px rgba(252, 252, 252, 0.8);
+  transform: scale(1.04166) !important;
+}
+
+.nav__item:active {
+  transform: scale(0.96) !important;
+}
+
+.nav__item__indicator {
+  position: relative;
+  width: 20px;
+  height: 3px;
+  border-radius: 5px;
+  left: calc(50% - 10px);
+  bottom: 0;
+  background-color: #999999;
+  margin: 2px 0;
+  user-select: none;
+}
+
+.nav__item:active > .nav__item__indicator {
+  background-color: #ed9c5a;
+  box-shadow: 0 0 6px 0 #ed9c5a;
 }
 
 .snackbar {
-  display: none;
-  min-width: 250px;
+  opacity: 0;
+  min-width: 160px;
   background-color: #efefef;
   color: #333;
   text-align: center;
@@ -93,25 +207,16 @@ export default {
   position: fixed;
   z-index: 1;
   left: 50%;
-  bottom: 30px;
+  bottom: 0;
   transform: translateX(-50%);
   font-weight: bold;
-  font-family: Poppins,Montserrat,serif;
+  font-family: Poppins, Montserrat, serif;
+  transition: opacity 0.5s linear, bottom 0.5s linear;
 }
 
 .snackbar.show {
-  display: block;
-  -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-  animation: fadein 0.5s, fadeout 0.5s 2.5s;
-}
-
-.nav__item:hover {
-  cursor: pointer;
-}
-
-.nav__item:active {
-  box-shadow: inset 6px 6px 10px rgba(94, 104, 121, 0.4),
-    inset -3px -3px 7px rgba(255, 255, 255, 0.8);
+  opacity: 1;
+  bottom: 30px;
 }
 
 .bg__inner {
@@ -119,31 +224,47 @@ export default {
   top: 0;
   left: 0;
   height: max(100%, 1500px);
-  width: max(100%, 300px);
+  width: max(100%, 200px);
+  user-select: none;
 }
 
-/*.parallax-bg {*/
-/*  position: absolute;*/
-/*  top: 0;*/
-/*  right: 0;*/
-/*  left: 0;*/
-/*  height: 2000px;*/
-/*  width: 100%;*/
-/*  transform: translateZ(-18px) scale(1.75);*/
-/*}*/
-
 .hero__wrapper {
-  height: 580px;
-  transform: translateZ(0);
+  height: min(580px, 150vw);
   display: flex;
   align-items: center;
   justify-content: center;
+  min-width: 200px;
 }
 
 .hero {
-  position: absolute;
-  width: min(92%, 1200px);
+  border-radius: 10px;
+  position: relative;
+  width: min(86%, 1200px);
   height: 100%;
+  transform: scale(0.98);
+}
+
+.hero.scale_and_fade_in {
+  animation: scalein 1s linear forwards;
+}
+
+.hero.scale_and_fade_in::before {
+  animation: fadein 1s linear forwards, scalein 1s linear forwards;
+}
+
+.hero::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  border-radius: 10px;
+  background-color: rgba(94, 104, 121, 0.05);
+  box-shadow: 12px 12px 20px rgba(94, 104, 121, 0.4),
+  -4px -4px 14px rgba(252, 252, 252, 0.8);
+  transform: scale(1.0204);
+  opacity: 0;
 }
 
 .hero__title {
@@ -152,9 +273,9 @@ export default {
   left: 65%;
   width: 50%;
   transform: translateX(-50%) translateY(-50%);
-  font-size: max(25px, min(6vw, 30px));
+  font-size: max(16px, min(8vw, 30px));
   color: #212131;
-  font-family: "Poppins Medium",Montserrat,serif;
+  font-family: "Poppins Medium", Montserrat, serif;
 }
 
 .hero__image__wrapper {
@@ -167,10 +288,38 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  user-select: none;
+  transform: scale(0.98);
+}
+
+.hero__image__wrapper.scale_and_fade_in {
+  animation: scalein 0.7s linear forwards;
+  animation-delay: 1s;
+}
+
+.hero__image__wrapper.scale_and_fade_in::before {
+  animation: fadein 0.7s ease-in forwards 1s, scalein 0.7s linear forwards 1s;
+}
+
+.hero__image__wrapper::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  border-radius: 50%;
+  background-color: rgba(94, 104, 121, 0.05);
+  box-shadow: 8px 8px 14px rgba(94, 104, 121, 0.4),
+  -3px -3px 8px rgba(252, 252, 252, 0.8);
+  opacity: 0;
+  transform: scale(1);
 }
 
 .hero__image {
   width: 80%;
+  user-select: none;
+  pointer-events: none;
 }
 
 @media screen and (max-width: 1100px) {
@@ -181,11 +330,11 @@ export default {
   }
 
   .hero__image__wrapper {
-    height: min(240px, 80vw);
-    width: min(240px, 80vw);
-    top: 12%;
-    left: 50%;
-    transform: translateX(-50%);
+    height: min(240px, 60vw);
+    width: min(240px, 60vw);
+    top: 20%;
+    left: calc(50% - min(120px, 30vw));
+    transform: scale(0.98);
   }
 }
 
@@ -209,6 +358,7 @@ export default {
   width: 10em;
   height: 10em;
 }
+
 .loader {
   font-size: 10px;
   border-top: 1.1em solid rgba(255, 255, 255, 0.2);
@@ -218,6 +368,7 @@ export default {
   -webkit-animation: load8 1.1s infinite linear;
   animation: load8 1.1s infinite linear;
 }
+
 @-webkit-keyframes load8 {
   0% {
     -webkit-transform: rotate(0deg);
@@ -228,6 +379,7 @@ export default {
     transform: rotate(360deg);
   }
 }
+
 @keyframes load8 {
   0% {
     -webkit-transform: rotate(0deg);
@@ -238,23 +390,40 @@ export default {
     transform: rotate(360deg);
   }
 }
+
 @-webkit-keyframes fadein {
-  from {bottom: 0; opacity: 0;}
-  to {bottom: 30px; opacity: 1;}
+  to {
+    opacity: 1;
+  }
 }
 
 @keyframes fadein {
-  from {bottom: 0; opacity: 0;}
-  to {bottom: 30px; opacity: 1;}
+  to {
+    opacity: 1;
+  }
 }
 
 @-webkit-keyframes fadeout {
-  from {bottom: 30px; opacity: 1;}
-  to {bottom: 0; opacity: 0;}
+  to {
+    opacity: 0;
+  }
 }
 
 @keyframes fadeout {
-  from {bottom: 30px; opacity: 1;}
-  to {bottom: 0; opacity: 0;}
+  to {
+    opacity: 0;
+  }
+}
+
+@keyframes scalein {
+  to {
+    transform: scale(1);
+  }
+}
+
+@-webkit-keyframes scalein {
+  to {
+    transform: scale(1);
+  }
 }
 </style>
