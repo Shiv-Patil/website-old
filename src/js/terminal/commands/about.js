bb8c1184b6c8ae67ae552157000aec9821738300
KrYmZiN <35653831+Shiv-Patil@users.noreply.github.com>
2021-11-12 12:38:26 +0530

    about section

--- src/js/terminal/commands/about.js
+++ src/js/terminal/commands/about.js
@@ -3,16 +3,12 @@ let exports = {};
 exports.aboutCommand = () => {
   const age = new Date().getFullYear() - 2005;
   return (
-    "\nHey! My name is Shiv Patil, and I am an Indian student programmer. \
-I'm roughly " +
+    "Hey there! My name is Shiv, and I'm currently " +
     age.toString() +
-    " years old and have been programming for 4 years.\n\n\
-I've largely worked with Python and am quite acquainted with it.\n\
-The Kivy GUI framework in Python was used in the majority of my projects, \
-and I've also completed several freelancing tasks with it.\n\n\
-Aside from Python, I've experimented in other languages like C# and C++ and have some beginner-level knowledge with them.\n\
-I am also well-versed in JavaScript.\n\n\
-Discord ID: KrYmZiN#0311\n"
+    ".\nI'm a self-taught programmer who is well-acquainted with python and has some experience with JavaScript and C++.\n" +
+    "I have largely worked with the Kivy python framework to make cross-platform GUI apps.\n" +
+    "Currently I am trying to learn new technologies and trying out different programming languages to work with.\n" +
+    "I like rock/metal music :P\n"
   );
 };
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,8 @@
   />
   <div class="nav">
     <router-link class="nav__item neu" to="/" @click="nav_item_click(this)"
-    >Terminal
-      <div class="nav__item__indicator"></div
-      >
+      >Terminal
+      <div class="nav__item__indicator"></div>
     </router-link>
     <div
       class="nav__item neu"
@@ -42,7 +41,7 @@
   </div>
   <div class="hero__wrapper">
     <div class="hero neu">
-      <div class="hero__title">Website work in progress :)</div>
+      <div class="hero__title">Krymzin</div>
       <div class="hero__image__wrapper neu">
         <img
           src="@/assets/hero__image.png"
@@ -50,6 +49,50 @@
           class="hero__image"
           alt="picture"
         />
+      </div>
+    </div>
+  </div>
+  <div class="about__wrapper">
+    <div class="about neu" id="about">
+      <div class="about__text__wrapper">
+        <span class="about__text__title">About</span>
+        <div class="about__text">
+          Hey there! My name is Shiv, currently
+          {{ new Date().getFullYear() - 2005 }}. I'm a self-taught programmer
+          who is well-acquainted with python and has some experience with
+          JavaScript and C++.<br />
+          I have largely worked with the Kivy python framework to make
+          cross-platform GUI apps.<br />
+          Currently I am trying to learn new technologies and trying out
+          different programming languages to work with.<br />
+          I like rock/metal music :P
+        </div>
+      </div>
+      <div class="about__images__container">
+        <div class="python about__image__wrapper neu">
+          <img
+            src="@/assets/python_logo.png"
+            class="about__image"
+            alt="py logo"
+          />
+        </div>
+        <div class="js about__image__wrapper neu">
+          <img src="@/assets/js_logo.png" class="about__image" alt="js logo" />
+        </div>
+        <div class="cpp about__image__wrapper neu">
+          <img
+            src="@/assets/cpp_logo.png"
+            class="about__image"
+            alt="cpp logo"
+          />
+        </div>
+        <div class="kivy about__image__wrapper neu">
+          <img
+            src="@/assets/kivy_logo.png"
+            class="about__image"
+            alt="kivy logo"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -66,7 +109,7 @@ export default {
   data() {
     return {
       scrollEventTimeout: 400,
-      scrollEventLastCalled: Date.now()
+      scrollEventLastCalled: Date.now(),
     };
   },
   components: {},
@@ -84,10 +127,9 @@ export default {
       }, 2000);
     },
     aboutButton() {
-      window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+      document.getElementById("about").scrollIntoView();
     },
-    nav_item_click() {
-    },
+    nav_item_click() {},
     pageScroll() {
       if (Date.now() - this.scrollEventLastCalled > this.scrollEventTimeout) {
         this.pageScrollTimeout();
@@ -95,35 +137,29 @@ export default {
       }
     },
     pageScrollTimeout() {
-      console.log("timeout scroll");
       const vh = window.innerHeight;
       document.querySelectorAll(".neu").forEach((ele) => {
         if (!ele.classList.contains("scale_and_fade_in"))
-          if (ele.getBoundingClientRect().top < vh - 10)
+          if (ele.getBoundingClientRect().top < vh - 50)
             ele.classList.add("scale_and_fade_in");
       });
-    }
+    },
   },
   mounted() {
     window.addEventListener("scroll", this.pageScroll);
   },
   unmounted() {
     window.removeEventListener("scroll", this.pageScroll);
-  }
+  },
 };
 </script>
 
 <style scoped>
-.home {
-  min-width: 200px;
-}
-
 .nav {
   display: flex;
   justify-content: center;
-  padding: 5px;
-  margin: max(2px, min(0.5vw, 4px)) 0;
-  min-width: 180px;
+  margin: 12px 0;
+  width: 100%;
 }
 
 .scale_and_fade_in {
@@ -135,11 +171,11 @@ export default {
   transform: scale(0.97);
   font-size: max(8px, min(4vw, 16px));
   margin: 8px max(4px, min(2vw, 8px));
-  padding: max(4px, min(2vw, 8px));
+  padding: max(4px, min(2vw, 9px));
   user-select: none;
   color: #212131;
   text-decoration: none;
-  font-family: Poppins, Montserrat, serif;
+  font-family: Montserrat, sans-serif;
 }
 
 .nav__item::before {
@@ -151,7 +187,7 @@ export default {
   bottom: 0;
   left: 0;
   box-shadow: 4px 4px 8px rgba(94, 104, 121, 0.4),
-  -2px -2px 5px rgba(252, 252, 252, 0.8);
+    -2px -2px 5px rgba(252, 252, 252, 0.8);
   background-color: rgba(94, 104, 121, 0.05);
   opacity: 0;
   transform: scale(1.04166);
@@ -171,7 +207,7 @@ export default {
 
 .nav__item:active::before {
   box-shadow: inset 2px 2px 4px rgba(94, 104, 121, 0.4),
-  inset -2px -2px 4px rgba(252, 252, 252, 0.8);
+    inset -2px -2px 4px rgba(252, 252, 252, 0.8);
   transform: scale(1.04166) !important;
 }
 
@@ -181,13 +217,14 @@ export default {
 
 .nav__item__indicator {
   position: relative;
-  width: 20px;
+  width: max(12px, min(5vw, 20px));
   height: 3px;
   border-radius: 5px;
-  left: calc(50% - 10px);
+  left: calc(50% - max(6px, min(2.5vw, 10px)));
   bottom: 0;
   background-color: #999999;
-  margin: 2px 0;
+  margin-bottom: 2px;
+  margin-top: 4px;
   user-select: none;
 }
 
@@ -262,7 +299,7 @@ export default {
   border-radius: 10px;
   background-color: rgba(94, 104, 121, 0.05);
   box-shadow: 12px 12px 20px rgba(94, 104, 121, 0.4),
-  -4px -4px 14px rgba(252, 252, 252, 0.8);
+    -4px -4px 14px rgba(252, 252, 252, 0.8);
   transform: scale(1.0204);
   opacity: 0;
 }
@@ -273,9 +310,9 @@ export default {
   left: 65%;
   width: 50%;
   transform: translateX(-50%) translateY(-50%);
-  font-size: max(16px, min(8vw, 30px));
+  font-size: max(32px, min(14vw, 76px));
   color: #212131;
-  font-family: "Poppins Medium", Montserrat, serif;
+  font-family: "Dancing Script", Montserrat, serif;
 }
 
 .hero__image__wrapper {
@@ -311,7 +348,7 @@ export default {
   border-radius: 50%;
   background-color: rgba(94, 104, 121, 0.05);
   box-shadow: 8px 8px 14px rgba(94, 104, 121, 0.4),
-  -3px -3px 8px rgba(252, 252, 252, 0.8);
+    -3px -3px 8px rgba(252, 252, 252, 0.8);
   opacity: 0;
   transform: scale(1);
 }
@@ -335,6 +372,171 @@ export default {
     top: 20%;
     left: calc(50% - min(120px, 30vw));
     transform: scale(0.98);
+  }
+}
+
+.about__wrapper {
+  margin-top: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 200px;
+  height: min(580px, 150vw);
+}
+
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  position: relative;
+  width: min(86%, 1200px);
+  height: 100%;
+  transform: scale(0.98);
+}
+
+.about.scale_and_fade_in {
+  animation: scalein 1s linear forwards;
+}
+
+.about.scale_and_fade_in::before {
+  animation: fadein 1s linear forwards, scalein 1s linear forwards;
+}
+
+.about::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  border-radius: 10px;
+  background-color: rgba(94, 104, 121, 0.05);
+  box-shadow: 12px 12px 20px rgba(94, 104, 121, 0.4),
+    -4px -4px 14px rgba(252, 252, 252, 0.8);
+  transform: scale(1.0204);
+  opacity: 0;
+}
+
+.about__images__container {
+  display: grid;
+  column-gap: 20px;
+  row-gap: 20px;
+  grid-template-columns: auto auto;
+  justify-content: center;
+  align-items: center;
+  width: min(300px, 60%);
+  margin-bottom: min(10%, 40px);
+  margin-right: 10px;
+}
+
+.python {
+  margin-bottom: 20px;
+}
+
+.cpp {
+  margin-top: -40px;
+}
+
+.js {
+  margin-top: 20px;
+}
+
+.about__image__wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  transform: scale(0.98);
+  border-radius: 50%;
+  height: 0;
+  width: min(110px, 100%);
+  padding-bottom: min(110px, 100%);
+}
+
+.about__image__wrapper.scale_and_fade_in {
+  animation: scalein 0.7s linear forwards;
+  animation-delay: 1s;
+}
+
+.about__image__wrapper.scale_and_fade_in::before {
+  animation: fadein 0.7s ease-in forwards 1s, scalein 0.7s linear forwards 1s;
+}
+
+.about__image__wrapper::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  border-radius: 50%;
+  background-color: rgba(94, 104, 121, 0.05);
+  box-shadow: 8px 8px 14px rgba(94, 104, 121, 0.4),
+    -3px -3px 8px rgba(252, 252, 252, 0.8);
+  opacity: 0;
+  transform: scale(1);
+}
+
+.cpp.about__image__wrapper::before {
+  content: "";
+  border-radius: 0;
+}
+
+.js.about__image__wrapper::before {
+  content: "";
+  border-radius: 0;
+}
+
+.about__image {
+  width: 80%;
+  margin-top: 100%;
+  user-select: none;
+  pointer-events: none;
+}
+
+.about__text__wrapper {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+
+.about__text__title {
+  font-size: max(20px, min(9vw, 48px));
+  color: #212131;
+  font-family: "Poppins Light", Montserrat, serif;
+}
+
+.about__text {
+  font-size: max(11px, min(3.6vw, 24px));
+  color: #212131;
+  font-family: Montserrat, serif;
+  text-align: left;
+  padding: min(3vw, 40px);
+}
+
+@media screen and (max-width: 1100px) {
+  .about__wrapper {
+    height: min(750px, 150vw);
+  }
+
+  .about {
+    flex-direction: column;
+  }
+
+  .about__text {
+    text-align: center;
+  }
+
+  .about__images__container {
+    margin-right: 0;
+  }
+}
+
+@media screen and (max-width: 280px) {
+  .about__wrapper {
+    height: calc(700px - 100vw);
   }
 }
 
